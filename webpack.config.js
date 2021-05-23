2f0ced9ed2c18be21bc031f0a53e12e947d8486e
ChristaPigeon <cstath26@gmail.com>
2021-05-22 17:41:22 -0700

    feat: Add output to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,9 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: './src/client/index.js',
+  output: {
+   path: path.resolve(__dirname, './dist'),
+   filename: 'bundle.js'
+ },
+
 }
