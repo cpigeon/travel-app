0ef66c1c44f22d6a62bd596ca558435208206078
ChristaPigeon <cstath26@gmail.com>
2021-05-23 10:55:47 -0700

    feat: Split webpack config file into dev & prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  mode: 'production',
   output: {
    path: path.resolve(__dirname, './dist'),
    filename: 'bundle.js'
